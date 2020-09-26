4c1cf7ee12b70bbd8dc2cc838c38e8f1da516eca
akshaya <akshay.algeri@ecanarys.com>
2020-09-26 17:18:38 +0530

    adding self hosted

---
+++ app_pool.ps1
@@ -0,0 +1,23 @@
+param ($pool_name, $app_name, $workspace)
+
+#check if the app pool exists
+if (!(Test-Path IIS:\AppPools\$pool_name ))
+{
+    #create the app pool
+    New-WebAppPool -Name $pool_name -Force
+}
+else {
+    echo "Pool exists"
+}
+
+#navigate to the sites root
+# cd IIS:\Sites\
+
+#check if the site exists
+if (!(Test-Path IIS:\Sites\$app_name ))
+{
+   New-Website -Name $app_name -ApplicationPool $iisAppPoolName -Force -PhysicalPath $workspace -Port 8084
+}
+else {
+    echo "site exists"
+}
